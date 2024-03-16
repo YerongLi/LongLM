84b6635a9f9b98fd61ea16d4f23828c884573bb7
Yerong Li <yerong.li@outlook.com>
2024-03-15 22:30:10 -0500

    Upload bash script for long prediction

--- llama_example.py
+++ llama_example.py
@@ -35,6 +35,7 @@ for line in open("passkey_examples_5k.jsonl", "r"):
 
     modify_method_of_instance(model, "LlamaAttention", "forward", original_llama_forward)
     tokens = model.generate(input_ids, max_new_tokens=6)
+    print(input_ids)
     answer= "Llama2:     [" + prompt_postfix + tokenizer.decode(tokens[0].tolist()[input_ids.shape[1]:], skip_special_tokens=True)  + "]"
     answer = answer.replace("\n", "\\n")
     print( answer )
